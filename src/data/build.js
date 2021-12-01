233bd354f8b380859e976a15c9eafa5b08b8fe71
Ulysse McConnell <ulysse.mcconnell+dev@protonmail.com>
2021-12-01 16:14:12 +0100

    Save current git commit to build info

--- src/data/build.js
+++ src/data/build.js
@@ -1,4 +1,16 @@
+const { execSync } = require('child_process')
+
+function getCurrentCommit(short = false) {
+    return execSync(`git rev-parse ${short ? '--short' : ''} HEAD`, {
+        cwd: __dirname
+    })
+        .toString()
+        .trim()
+}
+
 module.exports = {
     env: process.env.ELEVENTY_ENV,
-    timestamp: new Date()
+    timestamp: new Date(),
+    gitCommitHash: getCurrentCommit(),
+    gitCommitHashShort: getCurrentCommit(true)
 }
